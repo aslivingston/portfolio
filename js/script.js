d3bacfe0e7ca31cb209560b5a363bb6673c2c647
Alex Livingston <aslivingston@hotmail.co.uk>
2024-01-30 17:12:09 +0000

    json file projectPage link added

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ fetch('js/data.json').then(function (response) {
             </div>
 
             <div class="project-text">
-            <a class="project-links" href="${project.portfolioPage}"><h3 class="box">&nbsp;${project.name}</h3></a>
+            <a class="projectPage" href="${project.portfolioPage}"><h3 class="box">&nbsp;${project.name}</h3></a>
             <p>${project.desc}</p>
             
             <div class="project-links">
