922ceee34fd6fe40194eab7501085745446e9e51
Alex Livingston <aslivingston@hotmail.co.uk>
2024-02-01 17:16:05 +0000

    click handler and modal added and linked

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// -------------- JSON Project File Fetch --------------------------
+// -------------- Project Section - JSON Project File Fetch --------------------------
 
 const projectsDisplay = document.querySelector('.portfolio')
 
@@ -7,25 +7,67 @@ fetch('js/data.json').then(function (response) {
 }).then(function (data) {
     //console.log(data)
     data.projects.forEach(function (project) {
-        console.log(project)
+        // console.log(project)
         projectsDisplay.innerHTML += `
             <div>
                 <p class="numbers">${project.id} - ${project.name}</p>
             </div>
 
             <div class="project-links">
-                <button>View Case Study</button>
+                <a class="project-button" href="">View Case Study</a>
                 <a class="web-links" href="${project.github}" target="_blank" ><i class="fa-brands fa-github fa-2xl"></i>Github</a>
                 <a class="web-links" href="${project.liveLink}" target="_blank" ><i class="fa-solid fa-up-right-from-square fa-xl"></i>Live Website</a>
             </div>
 
                 <div class="project-img-div">
                     <img class="project-image" src="${project.img}" alt="example project image">
-                </div>
-                
-`
+                </div>`
+
+
+    })
+
+    function clickHandler(e) {
+        //console.log('clicked')
+        e.preventDefault()
+        document.querySelector('.module').classList.toggle('visible')
+    }
+
+    //document.querySelector('.project-button').addEventListener('click', clickHandler)
+    
+    document.querySelectorAll('.project-button').forEach(button => {
+        button.addEventListener('click', clickHandler)
     })
 })
+
+
+//---------------- Pop Up - JSON File Fetch --------------
+
+// const projectsName = document.querySelector('.pop-up-title')
+
+// fetch('js/data.json').then(function (response) {
+//     return response.json() 
+// }).then(function (data) {
+//     console.log(data)
+//     data.projects.forEach(function (project) {
+//         console.log(project)
+//         projectsName.innerHTML += `
+//         <div class="pop-up-left">
+//             <span class="pop-up-title">${project.name}</span>    
+//         </div>
+//             `
+//     })
+
+// })
+
+function closeHandler(e) {
+    //console.log('clicked')
+    e.preventDefault()
+    document.querySelector('.module').classList.toggle('visible')
+}
+
+document.querySelector('.cancel').addEventListener('click', closeHandler)
+
+
 
 //---------- Contact Form Submit Handler --------------------------
 
