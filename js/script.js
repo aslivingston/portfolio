9a2819340c7080ef24b76e92c369c532da02585f
alexlivingston <aslivingston@hotmail.co.uk>
2025-05-05 10:10:29 +0100

    styling and animations

--- js/script.js
+++ js/script.js
@@ -1,34 +1,52 @@
 // -------------- INDEX Project Section - JSON Project File Fetch --------------------------
 
-const projectsDisplay = document.querySelector('.portfolio')
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
 
-fetch('js/data.json').then(function (response) {
-    return response.json() 
-}).then(function (data) {
-    //console.log(data)
-    data.projects.forEach(function (project) {
-        // console.log(project)
-        projectsDisplay.innerHTML += `
-            <div>
-                <p class="numbers">${project.id} - ${project.name}</p>
-            </div>
+    // Observe all existing .entry-animation elements
+    document.querySelectorAll('.entry-animation').forEach(el => {
+        observer.observe(el);
+    });
 
-            <div class="project-links">
-                ${project.github ? `<a class="web-links" href="${project.github}" target="_blank"><i class="fa-brands fa-github fa-2xl"></i>Github</a>` : ''}
-                ${project.liveLink ? `<a class="web-links" href="${project.liveLink}" target="_blank"><i class="fa-solid fa-up-right-from-square fa-xl"></i>Live Website</a>` : ''}
-                ${project.tag1 ? `<button class="projectTags" type="button" disabled>${project.tag1}</button>` : ''}
-                ${project.tag2 ? `<button class="projectTags" type="button" disabled>${project.tag2}</button>` : ''}
-                ${project.tag3 ? `<button class="projectTags" type="button" disabled>${project.tag3}</button>` : ''}
-                ${project.team ? `<button class="teamTag" type="button" disabled>${project.team}</button>` : ''}
-            </div>
+    // Fetch and add projects dynamically
+    fetch('js/data.json')
+        .then(response => response.json())
+        .then(data => {
+            data.projects.forEach(project => {
+                const wrapper = document.createElement('div');
+                wrapper.classList.add('entry-animation');
 
-            <div class="project-img-div">
-                <p class="project-details" >${project.desc}</p>
-                <img class="project-image" src="${project.img}" alt="example project image">
-            </div>`
+                wrapper.innerHTML = `
+                    <p class="numbers">${project.id} - ${project.name}</p>
 
-    })
-})
+                    <div class="project-links">
+                        ${project.github ? `<a class="web-links" href="${project.github}" target="_blank"><i class="fa-brands fa-github fa-2xl"></i>Github</a>` : ''}
+                        ${project.liveLink ? `<a class="web-links" href="${project.liveLink}" target="_blank"><i class="fa-solid fa-up-right-from-square fa-xl"></i>Live Website</a>` : ''}
+                        ${project.tag1 ? `<button class="projectTags" type="button" disabled>${project.tag1}</button>` : ''}
+                        ${project.tag2 ? `<button class="projectTags" type="button" disabled>${project.tag2}</button>` : ''}
+                        ${project.tag3 ? `<button class="projectTags" type="button" disabled>${project.tag3}</button>` : ''}
+                        ${project.team ? `<button class="teamTag" type="button" disabled>${project.team}</button>` : ''}
+                    </div>
+
+                    <div class="project-img-div">
+                        <p class="project-details">${project.desc}</p>
+                        <img class="project-image" src="${project.img}" alt="example project image">
+                    </div>`;
+
+                projectsDisplay.appendChild(wrapper);
+                observer.observe(wrapper); // Observe dynamic entry
+            });
+        });
+});
+
     // function clickHandler(e) {
     //     //console.log('clicked')
     //     e.preventDefault()
@@ -84,3 +102,4 @@ function submitHandler(event) {
     }
 
 }
+
