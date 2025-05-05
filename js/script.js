44665a6c5845944d91de7df6d2ab4fc28e243ddd
alexlivingston <aslivingston@hotmail.co.uk>
2025-05-05 10:44:07 +0100

    entry animations

--- js/script.js
+++ js/script.js
@@ -1,52 +1,54 @@
 // -------------- INDEX Project Section - JSON Project File Fetch --------------------------
 
+const projectsDisplay = document.querySelector('.portfolio')
+
+fetch('js/data.json').then(function (response) {
+    return response.json() 
+}).then(function (data) {
+    //console.log(data)
+    data.projects.forEach(function (project) {
+        // console.log(project)
+        projectsDisplay.innerHTML += `
+            <div>
+                <p class="numbers">${project.id} - ${project.name}</p>
+            </div>
+
+            <div class="project-links">
+                ${project.github ? `<a class="web-links" href="${project.github}" target="_blank"><i class="fa-brands fa-github fa-2xl"></i>Github</a>` : ''}
+                ${project.liveLink ? `<a class="web-links" href="${project.liveLink}" target="_blank"><i class="fa-solid fa-up-right-from-square fa-xl"></i>Live Website</a>` : ''}
+                ${project.tag1 ? `<button class="projectTags" type="button" disabled>${project.tag1}</button>` : ''}
+                ${project.tag2 ? `<button class="projectTags" type="button" disabled>${project.tag2}</button>` : ''}
+                ${project.tag3 ? `<button class="projectTags" type="button" disabled>${project.tag3}</button>` : ''}
+                ${project.team ? `<button class="teamTag" type="button" disabled>${project.team}</button>` : ''}
+            </div>
+
+            <div class="project-img-div">
+                <p class="project-details" >${project.desc}</p>
+                <img class="project-image" src="${project.img}" alt="example project image">
+            </div>`
+
+    })
+})
+
 document.addEventListener("DOMContentLoaded", function () {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('active');
-            }
-        });
-    }, {
-        threshold: 0.2
-    });
-
-    // Observe all existing .entry-animation elements
-    document.querySelectorAll('.entry-animation').forEach(el => {
-        observer.observe(el);
-    });
-
-    // Fetch and add projects dynamically
-    fetch('js/data.json')
-        .then(response => response.json())
-        .then(data => {
-            data.projects.forEach(project => {
-                const wrapper = document.createElement('div');
-                wrapper.classList.add('entry-animation');
-
-                wrapper.innerHTML = `
-                    <p class="numbers">${project.id} - ${project.name}</p>
-
-                    <div class="project-links">
-                        ${project.github ? `<a class="web-links" href="${project.github}" target="_blank"><i class="fa-brands fa-github fa-2xl"></i>Github</a>` : ''}
-                        ${project.liveLink ? `<a class="web-links" href="${project.liveLink}" target="_blank"><i class="fa-solid fa-up-right-from-square fa-xl"></i>Live Website</a>` : ''}
-                        ${project.tag1 ? `<button class="projectTags" type="button" disabled>${project.tag1}</button>` : ''}
-                        ${project.tag2 ? `<button class="projectTags" type="button" disabled>${project.tag2}</button>` : ''}
-                        ${project.tag3 ? `<button class="projectTags" type="button" disabled>${project.tag3}</button>` : ''}
-                        ${project.team ? `<button class="teamTag" type="button" disabled>${project.team}</button>` : ''}
-                    </div>
-
-                    <div class="project-img-div">
-                        <p class="project-details">${project.desc}</p>
-                        <img class="project-image" src="${project.img}" alt="example project image">
-                    </div>`;
-
-                projectsDisplay.appendChild(wrapper);
-                observer.observe(wrapper); // Observe dynamic entry
-            });
-        });
-});
-
+    const animatedElements = document.querySelectorAll(".entry-animation");
+  
+    function checkAnimations() {
+      animatedElements.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        const triggerPoint = window.innerHeight * 0.9;
+  
+        if (rect.top < triggerPoint) {
+          el.classList.add("active");
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", checkAnimations);
+    window.addEventListener("resize", checkAnimations); // optional
+    checkAnimations(); // initial check in case elements are already in view
+  });
+  
     // function clickHandler(e) {
     //     //console.log('clicked')
     //     e.preventDefault()
